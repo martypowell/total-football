494cb4eb69e9a4e2b4d00b0c9b27c963dde020f3
Marty Powell <jpowel25@gmail.com>
2018-01-22 21:01:18 -0500

    added standings component

---
+++ src/shared/Standings.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './standings.css';
+
+class Standings extends Component {
+  render() {
+    return (
+      <div className="standings">
+        <h1>Standings Headline goes here</h1>
+      </div>
+    );
+  }
+}
+
+export default Standings;
